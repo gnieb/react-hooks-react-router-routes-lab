565a90fa65abc80497010d2868af71c4ec82de94
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-02 16:57:55 -0500

    all core deliverables completed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+const displayMovies = movies.map(movie => {
+  return (
+    <div key={movie.title}>
+      <h2>Name: {movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <ul>{movie.genres.map(genre => {
+        return (
+          <li key={genre}>{genre}</li>
+        )
+      })}</ul>
+
+    </div>
+  )
+})
+
+  return (
+    <div>
+    <h1>Movies Page</h1>
+    {displayMovies}
+    </div>
+    )
+  
 }
 
 export default Movies;
